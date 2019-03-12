cd691bec90eac39a441cf5f18bacc279a8a9ddbe
Ahmed Saeed <saeedal@umich.edu>
2019-03-12 01:33:52 -0400

    added update, insert, and delete queries on the brand page. brand page complete.

--- 421/admin/brands.php
+++ 421/admin/brands.php
@@ -2,63 +2,99 @@
 require_once '../core/init.php';
 include 'includes/head.php';
 include 'includes/navigation.php';
-
-//get brands from database
-$sql = "SELECT * FROM brand ORDER BY brand";
-$results = $db->query($sql);
+$results = $db->query("SELECT * FROM brand ORDER BY brand");
 $errors = array();
-    // If add form is submitted
-    if (isset($_POST['add_submit'])) {
-        // protect against sql injection, and then set string
-        $brand = sanitize($_POST['brand']);
-        // check if brand is blank
-        if ($_POST['brand'] == '') {
-            $errors[] .= 'You must enter a brand!';
-        }
-        //check if brand exists in database
-        $sql = "SELECT * FROM brand WHERE brand = '$brand'";
-        $result = $db->query($sql);
-        $count = mysqli_num_rows($result);
-        if($count > 0){
-            $errors[] .= $brand.' already exists. Please choose another brand name. . .';
-        }
-
-        // display errors
-        if (!empty($errors)) {
-            echo display_errors($errors);
-        } else {
-            // Add brand in database
-            $sql = "INSERT INTO brand (brand) VALUES ('$brand')";
-            $db->query($sql);
-            header('Location: brands.php');
-        }
+// Edit brand
+if (isset($_GET['edit']) && !empty($_GET['edit'])) {
+    $edit_id = (int)$_GET['edit'];
+    $edit_id = sanitize($edit_id);
+    $edit_result = $db->query("SELECT * FROM brand WHERE id = '{$edit_id}'");
+    $eBrand = mysqli_fetch_assoc($edit_result);
+}
+// Delete brand
+if (isset($_GET['delete']) && !empty($_GET['delete'])) {
+    $delete_id = (int)$_GET['delete'];
+    $delete_id = sanitize($delete_id);
+    $db->query("DELETE FROM brand WHERE id = '{$delete_id}'");
+    header("Location: brands.php");
+}
+if (isset($_POST['add_submit'])) {
+    $brand = sanitize($_POST['brand']);
+    // Check if brand is blank
+    if ($brand == '') {
+        $errors[] .= 'You must enter a brand!';
     }
+    // Check if brand exist in database
+    $sql = "SELECT * FROM brand WHERE brand = '{$brand}'";
+    if (isset($_GET['edit'])) {
+        $sql = "SELECT * FROM brand WHERE brand = '{$brand}' AND id != '{$edit_id}'";
+    }
+    $result = $db->query($sql);
+    $count = mysqli_num_rows($result);
+    if ($count > 0) {
+        $errors[] .= $brand . ' already exist. Please choose another brand name.';
+    }
+    // Display errors
+    if (!empty($errors)) {
+        echo display_errors($errors);
+    } else {
+        // Add brand to database
+        $sql = "INSERT INTO brand (brand) VALUES ('{$brand}')";
+        if (isset($_GET['edit'])) {
+            $sql = "UPDATE brand SET brand = '{$brand}' WHERE id = '{$edit_id}'";
+        }
+        $db->query($sql);
+        header('Location: brands.php');
+    }
+}
 ?>
+    <h2 class="text-center">Brands</h2>
+    <hr>
 
-<h2 class="text-center">Brands</h2><hr>
-<!-- Brand Form -->
-<div class="text-center">
-    <form class="form-inline" action="brands.php" method="post">
-        <div class="form-group">
-            <label for="brand">Add a Brand:</label>
-            <input type="text" name="brand" id="brand" class="form-control" value="<?= ((isset($_POST['brand'])) ? $_POST['brand'] : ''); ?>">
-            <input type="submit" name="add_submit" value="Add Brand" class="btn btn-success">
-        </div>
-    </form>
-</div>
-<hr>
-<table class="table table-auto table-bordered table-striped table-hover" style="margin: 0 auto; width: auto;">
-    <thead>
-    <th></th><th>Brand</th><th></th>
-    </thead>
-    <tbody>
-    <?php while($brand = mysqli_fetch_assoc($results)) : ?>
-        <tr>
-            <td><a href="brands.php?edit=<?= $brand['id']; ?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil"></span></a></td>
-            <td><?= $brand['brand']; ?></td>
-            <td><a href="brands.php?delete=<?= $brand['id']; ?>" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-remove-sign"></span></a></td>
-        </tr>
-    <?php endwhile; ?>
-    </tbody>
-</table>
-<?php include 'includes/footer.php'; ?>
+    <div class="text-center">
+        <form class="form-inline" action="brands.php<?php echo((isset($_GET['edit'])) ? '?edit=' . $edit_id : ''); ?>"
+              method="post">
+            <div class="form-group">
+                <label for="brand"><?php echo((isset($_GET['edit'])) ? 'Edit' : 'Add A'); ?> Brand:</label>
+                <?php
+                $brand_value = '';
+                if (isset($_GET['edit'])) {
+                    $brand_value = $eBrand['brand'];
+                } else {
+                    if (isset($_POST['brand'])) {
+                        $brand_value = sanitize($_POST['brand']);
+                    }
+                }
+                ?>
+                <input class="form-control" type="text" name="brand" id="brand" value="<?php echo $brand_value; ?>">
+                <?php if (isset($_GET['edit'])) : ?>
+                    <a class="btn btn-default" href="brands.php">Cancel</a>
+                <?php endif; ?>
+                <input class="btn btn-success" type="submit" name="add_submit"
+                       value="<?php echo((isset($_GET['edit'])) ? 'Edit' : 'Add'); ?> Brand">
+            </div>
+        </form>
+    </div>
+    <hr>
+
+    <table class="table table-bordered table-striped table-auto table-condensed" style="width: auto; margin: 0 auto;">
+        <thead>
+        <th></th>
+        <th>Brand</th>
+        <th></th>
+        </thead>
+        <tbody>
+        <?php while ($brand = mysqli_fetch_assoc($results)) : ?>
+            <tr>
+                <td><a class="btn btn-xs btn-default" href="brands.php?edit=<?php echo $brand['id']; ?>"><span
+                                class="glyphicon glyphicon-pencil"></span></a></td>
+                <td><?php echo $brand['brand']; ?></td>
+                <td><a class="btn btn-xs btn-default" href="brands.php?delete=<?php echo $brand['id']; ?>"><span
+                                class="glyphicon glyphicon-remove-sign"></span></a></td>
+            </tr>
+        <?php endwhile; ?>
+        </tbody>
+    </table>
+
+<?php
+include 'includes/footer.php';
