414782a15ab448691d4f54bcee70bed6787c4649
Ahmed Saeed <saeedal@umich.edu>
2019-03-19 00:32:33 -0400

    added API for stripe implementation using text key, to publish activate stripe account and use the live api key and change the api key from github

--- 421/cart.php
+++ 421/cart.php
@@ -4,7 +4,7 @@ include 'includes/head.php';
 include 'includes/navigation.php';
 include 'includes/headerpartial.php';
 
-if($cart_id != ''){
+if ($cart_id != '') {
     $cartQ = $db->query("SELECT * FROM cart WHERE id = '{$cart_id}'");
     $result = mysqli_fetch_assoc($cartQ);
     $items = json_decode($result['items'], true);
@@ -18,8 +18,9 @@ if($cart_id != ''){
 
 <div class="col-md-12">
     <div class="row">
-        <h2 class="text-center">My Shopping Cart</h2><hr>
-        <?php if($cart_id == ''): ?>
+        <h2 class="text-center">My Shopping Cart</h2>
+        <hr>
+        <?php if ($cart_id == ''): ?>
             <div class="bg-danger">
                 <p class="text-center text-danger">
                     Your shopping cart is empty!
@@ -27,36 +28,49 @@ if($cart_id != ''){
             </div>
         <?php else: ?>
             <table class="table table-bordered table-condensed table-striped">
-                <thead><th>#</th><th>Item</th><th>Price</th><th>Quantity</th><th>Size</th><th>Sub Total</th></thead>
+                <thead>
+                <th>#</th>
+                <th>Item</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Size</th>
+                <th>Sub Total</th>
+                </thead>
                 <tbody>
                 <?php
-                foreach($items as $item){
+                foreach ($items as $item) {
                     $product_id = $item['id'];
                     $productQ = $db->query("SELECT * FROM products WHERE id = '{$product_id}'");
                     $product = mysqli_fetch_assoc($productQ);
-                    $sArray = explode(',',$product['sizes']);
-                    foreach($sArray as $sizeString){
-                        $s = explode(':',$sizeString);
-                        if($s[0] == $item['size']){
+                    $sArray = explode(',', $product['sizes']);
+                    foreach ($sArray as $sizeString) {
+                        $s = explode(':', $sizeString);
+                        if ($s[0] == $item['size']) {
                             $available = $s[1];
                         }
                     }
                     ?>
                     <tr>
-                        <td><?=$i;?></td>
-                        <td><?=$product['title'];?></td>
-                        <td><?=money($product['price']);?></td>
+                        <td><?= $i; ?></td>
+                        <td><?= $product['title']; ?></td>
+                        <td><?= money($product['price']); ?></td>
                         <td>
-                            <button class="btn btn-xs btn-default" onclick="update_cart('removeone','<?=$product['id'];?>', '<?=$item['size'];?>');">-</button>
-                            <?=$item['quantity'];?>
-                            <?php if($item['quantity'] < $available): ?>
-                                <button class="btn btn-xs btn-default" onclick="update_cart('addone','<?=$product['id'];?>', '<?=$item['size'];?>');">+</button>
+                            <button class="btn btn-xs btn-default"
+                                    onclick="update_cart('removeone','<?= $product['id']; ?>', '<?= $item['size']; ?>');">
+                                -
+                            </button>
+                            <?= $item['quantity']; ?>
+                            <?php if ($item['quantity'] < $available): ?>
+                                <button class="btn btn-xs btn-default"
+                                        onclick="update_cart('addone','<?= $product['id']; ?>', '<?= $item['size']; ?>');">
+                                    +
+                                </button>
                             <?php else: ?>
                                 <span class="text-danger">Max</span>
                             <?php endif; ?>
                         </td>
-                        <td><?=$item['size'];?></td>
-                        <td><?=money($item['quantity'] * $product['price']);?></td>
+                        <td><?= $item['size']; ?></td>
+                        <td><?= money($item['quantity'] * $product['price']); ?></td>
                     </tr>
                     <?php
                     $i++;
@@ -75,18 +89,24 @@ if($cart_id != ''){
             </table>
             <table class="table table-bordered table-condensed text-right">
                 <legend>Totals</legend>
-                <thead class="totals-table-header"><th  style="text-align: center;">Total Items</th><th style="text-align: center;">Sub Total</th><th style="text-align: center;">Tax</th><th style="text-align: center;">Grand Total</th></thead>
+                <thead class="totals-table-header">
+                <th style="text-align: center;">Total Items</th>
+                <th style="text-align: center;">Sub Total</th>
+                <th style="text-align: center;">Tax</th>
+                <th style="text-align: center;">Grand Total</th>
+                </thead>
                 <tbody>
                 <tr>
-                    <td><?=$item_count;?></td>
-                    <td><?=money($sub_total);?></td>
-                    <td><?=money($tax);?></td>
-                    <td class="bg-success"><?=money($grand_total);?></td>
+                    <td><?= $item_count; ?></td>
+                    <td><?= money($sub_total); ?></td>
+                    <td><?= money($tax); ?></td>
+                    <td class="bg-success"><?= money($grand_total); ?></td>
                 </tr>
                 </tbody>
             </table>
             <!-- Check out Button -->
-            <button type="button" class="btn btn-primary btn-md pull-right" data-toggle="modal" data-target="#checkoutModal">
+            <button type="button" class="btn btn-primary btn-md pull-right" data-toggle="modal"
+                    data-target="#checkoutModal">
                 <span class="glyphicon glyphicon-shopping-cart"></span> Check Out >>
             </button>
 
@@ -95,7 +115,8 @@ if($cart_id != ''){
                 <div class="modal-dialog modal-lg" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                                        aria-hidden="true">&times;</span></button>
                             <h4 class="modal-title" id="checkoutModalLabel">Shipping Address</h4>
                         </div>
                         <div class="modal-body">
@@ -157,8 +178,8 @@ if($cart_id != ''){
                                             <!-- PCI COMPLIANCE, DO NOT SEND NAME="" TO SERVER -->
                                             <select class="form-control" id="exp-month">
                                                 <option value=""></option>
-                                                <?php for($i = 1; $i<13; $i++): ?>
-                                                    <option value="<?=$i;?>"><?=$i;?></option>
+                                                <?php for ($i = 1; $i < 13; $i++): ?>
+                                                    <option value="<?= $i; ?>"><?= $i; ?></option>
                                                 <?php endfor; ?>
                                             </select>
                                         </div>
@@ -166,10 +187,10 @@ if($cart_id != ''){
                                             <label for="exp-year">Expire Year:</label>
                                             <!-- PCI COMPLIANCE, DO NOT SEND NAME="" TO SERVER -->
                                             <select class="form-control" id="exp-year">
-                                                    <option value=""></option>
-                                                <?php $yr = date("Y");?>
-                                                <?php for($i=0; $i<11; $i++): ;?>
-                                                    <option value="<?=$yr + $i;?>"><?=$yr + $i;?></option>
+                                                <option value=""></option>
+                                                <?php $yr = date("Y"); ?>
+                                                <?php for ($i = 0; $i < 11; $i++): ; ?>
+                                                    <option value="<?= $yr + $i; ?>"><?= $yr + $i; ?></option>
                                                 <?php endfor; ?>
                                             </select>
                                         </div>
@@ -178,9 +199,15 @@ if($cart_id != ''){
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary" onclick="check_address();" id="next_button">Next >></button>
-                            <button type="button" class="btn btn-primary" onclick="back_address();" id="back_button" style="display: none;"><< Back</button>
-                            <button type="submit" class="btn btn-primary" id="check_out_button" style="display: none;">Check Out >></button>
+                            <button type="button" class="btn btn-primary" onclick="check_address();" id="next_button">
+                                Next >>
+                            </button>
+                            <button type="button" class="btn btn-primary" onclick="back_address();" id="back_button"
+                                    style="display: none;"><< Back
+                            </button>
+                            <button type="submit" class="btn btn-primary" id="check_out_button" style="display: none;">
+                                Check Out >>
+                            </button>
                             </form>
                         </div>
                     </div>
@@ -190,46 +217,46 @@ if($cart_id != ''){
     </div>
 </div>
 <script>
-    function back_address(){
+    function back_address() {
         jQuery('#payment-errors').html("");
         jQuery('#step1').css("display", "block");
         jQuery('#step2').css("display", "none");
         jQuery('#next_button').css("display", "inline-block");
-        jQuery('#back_button').css("display" ,"none");
-        jQuery('#check_out_button').css("display" ,"none");
+        jQuery('#back_button').css("display", "none");
+        jQuery('#check_out_button').css("display", "none");
         jQuery('#checkoutModalLabel').html("Shipping Address");
     }
 
-    function check_address(){
+    function check_address() {
         var data = {
-            'full_name' : jQuery('#full_name').val(),
-            'email'     : jQuery('#email').val(),
-            'street'    : jQuery('#street').val(),
-            'street2'   : jQuery('#street2').val(),
-            'city'      : jQuery('#city').val(),
-            'state'     : jQuery('#state').val(),
-            'zip_code'  : jQuery('#zip_code').val(),
-            'country'   : jQuery('#country').val(),
+            'full_name': jQuery('#full_name').val(),
+            'email': jQuery('#email').val(),
+            'street': jQuery('#street').val(),
+            'street2': jQuery('#street2').val(),
+            'city': jQuery('#city').val(),
+            'state': jQuery('#state').val(),
+            'zip_code': jQuery('#zip_code').val(),
+            'country': jQuery('#country').val(),
         };
         jQuery.ajax({
-            url     : '/421/admin/parsers/check_address.php',
-            method  : 'POST',
-            data    : data,
-            success : function(data){
-                if(data != 'passed'){
+            url: '/421/admin/parsers/check_address.php',
+            method: 'POST',
+            data: data,
+            success: function (data) {
+                if (data != 'passed') {
                     jQuery('#payment-errors').html(data);
                 }
-                if(data == 'passed'){
+                if (data == 'passed') {
                     jQuery('#payment-errors').html("");
                     jQuery('#step1').css("display", "none");
                     jQuery('#step2').css("display", "block");
                     jQuery('#next_button').css("display", "none");
-                    jQuery('#back_button').css("display" ,"inline-block");
-                    jQuery('#check_out_button').css("display" ,"inline-block");
+                    jQuery('#back_button').css("display", "inline-block");
+                    jQuery('#check_out_button').css("display", "inline-block");
                     jQuery('#checkoutModalLabel').html("Enter Your Card Details");
                 }
             },
-            error   : function(){
+            error: function () {
                 alert("Something went wrong!");
             },
         });
@@ -237,6 +264,6 @@ if($cart_id != ''){
 </script>
 
 
-<?php include 'includes/footer.php';?>
+<?php include 'includes/footer.php'; ?>
 
 
--- 421/config.php
+++ 421/config.php
@@ -4,4 +4,19 @@ define('BASEURL', $_SERVER['DOCUMENT_ROOT'].'/421/');
 define('CART_COOKIE', 'CIS421_SAEED_QAYED_ASOUFY_ALHAIDI');
 // saves cookie on client computer for 30 days
 define('CART_COOKIE_EXPIRE',time() + (86400 * 30));
-define('TAXRATE', 0.06);
+define('TAXRATE', 0.06); // sales tax rate. Set to 0 if you aren't charging tax
+
+//for payment
+define('CURRENCY', 'usd');
+//change test to live when you are ready to go live
+define('CHECKOUTMODE', 'TEST');
+
+if(CHECKOUTMODE == 'TEST'){
+    define('STRIPE_PRIVATE','sk_test_3F5tOA0RFE7KwMDKTbZf1u2s');
+    define('STRIPE_PUBLIC','pk_test_KsNNPi8ZikvGrcnLpDadgHuc');
+}
+
+if(CHECKOUTMODE == 'LIVE'){
+    define('STRIPE_PRIVATE','xxx');
+    define('STRIPE_PUBLIC','xxx');
+}
